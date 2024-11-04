50c455eb25a6fcca739c257f2ed7a0c7c18d61fb
NivDorfman <nivdorf@gmail.com>
2024-11-04 22:02:41 +0200

    Update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,15 @@
 // Splits a restaurant bill evenly among three diners.
+import java.util.Scanner; 
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+
+     	    Scanner temp = new Scanner(System.in);
+            System.out.println("enter price");
+            int price = temp.nextInt();
+            // calculates the price each diner should pay
+            double cost = Math.ceil(((double) price)/args.length);
+            System.out.println("Dear " + args[0] + ", " + args[1] + ", and " + args[2] + ": pay " + cost + " shekels each");	
 	}
 }
