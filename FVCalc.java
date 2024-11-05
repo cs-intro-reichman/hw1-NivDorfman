5c427a979731e61be594934f1420e64f91e8f2c6
NivDorfman <nivdorf@gmail.com>
2024-11-05 17:44:22 +0200

    Update FVCalc.java

--- FVCalc.java
+++ FVCalc.java
@@ -9,6 +9,6 @@ public class FVCalc {
 		int year = Integer.parseInt(args[2]);
 		//calculates the future investment after x years by CurrentValue*(1+AnnualInterest)^year)
 		double FutureValue = CurrentValue*(Math.pow(1+AnnualInterest/100 , (double)year));
-		System.out.println("After " + year + " years, $" + CurrentValue + " saved at " + AnnualInterest + "% will yield $" + (int) FutureValue );
+		System.out.println("After " + year + " years, a $" + CurrentValue + " saved at " + AnnualInterest + "% will yield $" + (int) FutureValue );
 	}
 }
