1a1896cef4ce2f5fe93878a6ba6edf5cc2c9cf77
NivDorfman <nivdorf@gmail.com>
2024-11-05 17:48:47 +0200

    Update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -2,8 +2,6 @@
 import java.util.Scanner; 
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
             // calculates the price each diner should pay
       	    double cost = Math.ceil((Double.parseDouble(args[3]))/3); 
             System.out.println("Dear " + args[2] + ", " + args[1] + ", and " + args[0] + ": pay " + cost + " Shekels each");
