1d5e1fa60b105fcfd5643314d6701e9e5d6cec0a
NivDorfman <nivdorf@gmail.com>
2024-11-05 18:09:28 +0200

    Update NumWords.java

--- NumWords.java
+++ NumWords.java
@@ -2,6 +2,6 @@
 public class NumWords {
 	public static void main(String args[]) {
 			
-		System.out.println(Integer.parseInt(args[0])/100 + " hundreds, " + (Integer.parseInt(args[0])/10)%10 + " tens, and " + Integer.parseInt(args[0])%10 + " ones");
+		System.out.println(Integer.parseInt(args[0])/100 + " hundreds, " + (Integer.parseInt(args[0])/10)%10 + " tens, and " + Integer.parseInt(args[0])%10 + " ones.");
 	}
 }
